dbad0214527444c785b27dd5b84be22950f67945
Andreas Eriksson <ae225ak@student.lnu.se>
2024-10-08 10:46:30 +0200

    Refactor

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
+// Define the grid area
+
 const gridArea = document.querySelector('#gridArea');
 
+let squaresPerSide = 32; // Sets the grid size - to be prompted of user later
+let numberOfSquares = Math.pow(squaresPerSide, 2);
 
+// Constants
+
+const width = 960;
+const height = 960;
 
 gridArea.style.display = 'flex';
 gridArea.style.outline = '1px solid black';
 gridArea.style.flexWrap = 'wrap';
+gridArea.style.width = `${width}px`;
+gridArea.style.height = `${height}px`;
 
-gridArea.style.justifyContent = 'flex-start';
+// Create the grid items
 
-let squaresPerSide = 16; // Sets the grid size - to be prompted of user later
-
-gridArea.style.width = '960px'
-gridArea.style.height = '960px'
-
-for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
+for (let i = 0; i < numberOfSquares; i++) {
     const gridItem = document.createElement('div');
-    gridItem.style.flex = `1 0 ${960 / squaresPerSide}px`;
+    gridItem.style.flex = `1 0 ${width / squaresPerSide}px`;
     gridItem.style.outline = '1px solid black';
+
+    // Add event listener to change color on hover
+    gridItem.addEventListener('mouseover', () => {
+    gridItem.classList.add('hover')})
     
     gridArea.appendChild(gridItem);
-    gridItem.addEventListener('mouseover', () => {
-        gridItem.classList.add('hover')
-    })
+    
 }
 

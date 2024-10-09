c9a4a0853d3839f4036a87b9553ccfe9768a1f1e
Andreas Eriksson <ae225ak@student.lnu.se>
2024-10-09 19:36:25 +0200

    Refactor

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 
 const gridArea = document.querySelector('#gridArea');
 
-let squaresPerSide = 32; // Sets the grid size - to be prompted of user later
-let numberOfSquares = Math.pow(squaresPerSide, 2);
+// Default grid size
 
-// Constants
+let squaresPerSide = 16;
+
+// Constants - 960x960px grid
 
 const width = 960;
 const height = 960;
@@ -16,18 +17,46 @@ gridArea.style.flexWrap = 'wrap';
 gridArea.style.width = `${width}px`;
 gridArea.style.height = `${height}px`;
 
-// Create the grid items
 
-for (let i = 0; i < numberOfSquares; i++) {
-    const gridItem = document.createElement('div');
-    gridItem.style.flex = `1 0 ${width / squaresPerSide}px`;
-    gridItem.style.outline = '1px solid black';
+// Functions
 
-    // Add event listener to change color on hover
-    gridItem.addEventListener('mouseover', () => {
-    gridItem.classList.add('hover')})
-    
-    gridArea.appendChild(gridItem);
-    
+function drawGrid(squaresPerSide) {
+    for (let i = 0; i < squaresPerSide ** 2; i++) {
+        const gridItem = document.createElement('div');
+        gridItem.style.flex = `1 0 ${width / squaresPerSide}px`;
+        gridItem.style.outline = '1px solid black';
+
+        // Add event listener to change color on hover
+        gridItem.addEventListener('mouseover', () => {
+            gridItem.classList.add('hover');
+        });
+
+        gridArea.appendChild(gridItem);
+    }
 }
+
+function removeGrid() {
+    while (gridArea.firstChild) {
+        gridArea.removeChild(gridArea.firstChild);
+    }
+}
+
+// Create the grid items with default number of squares
+
+drawGrid(squaresPerSide);
+
+const btn = document.createElement('button');
+btn.textContent = 'Squares per side?';
+btn.addEventListener('click', () => {
+   squaresPerSide = prompt('How many squares per side?');
+   removeGrid();
+   drawGrid(squaresPerSide);
+   
+})
+
+const page = document.querySelector('body');
+page.insertBefore(btn, gridArea);
+
+
+
 

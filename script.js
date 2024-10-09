3c372c4c3e2bdcf6a9198c74d3f6403f19e64e09
Andreas Eriksson <ae225ak@student.lnu.se>
2024-10-09 19:44:26 +0200

    Add check for maximum grid size

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 // Define the grid area
 
-const gridArea = document.querySelector('#gridArea');
+const gridArea = document.querySelector('#gridArea')
 
 // Default grid size
 
-let squaresPerSide = 16;
+let squaresPerSide = 16
 
 // Constants - 960x960px grid
 
-const width = 960;
-const height = 960;
+const width = 960
+const height = 960
 
-gridArea.style.display = 'flex';
-gridArea.style.outline = '1px solid black';
-gridArea.style.flexWrap = 'wrap';
-gridArea.style.width = `${width}px`;
-gridArea.style.height = `${height}px`;
-
+gridArea.style.display = 'flex'
+gridArea.style.outline = '1px solid black'
+gridArea.style.flexWrap = 'wrap'
+gridArea.style.width = `${width}px`
+gridArea.style.height = `${height}px`
 
 // Functions
 
@@ -25,46 +24,50 @@ gridArea.style.height = `${height}px`;
  * @param {*} squaresPerSide - Number of squares per side
  */
 function drawGrid(squaresPerSide) {
-    for (let i = 0; i < squaresPerSide ** 2; i++) {
-        const gridItem = document.createElement('div');
-        gridItem.style.flex = `1 0 ${width / squaresPerSide}px`;
-        gridItem.style.outline = '1px solid black';
+	for (let i = 0; i < squaresPerSide ** 2; i++) {
+		const gridItem = document.createElement('div')
+		gridItem.style.flex = `1 0 ${width / squaresPerSide}px`
+		gridItem.style.outline = '1px solid black'
 
-        // Add event listener to change color on hover
-        gridItem.addEventListener('mouseover', () => {
-            gridItem.classList.add('hover');
-        });
+		// Add event listener to change color on hover
+		gridItem.addEventListener('mouseover', () => {
+			gridItem.classList.add('hover')
+		})
 
-        gridArea.appendChild(gridItem);
-    }
+		gridArea.appendChild(gridItem)
+	}
 }
 
 /**
  * @description - Remove all grid items
  */
 function removeGrid() {
-    while (gridArea.firstChild) {
-        gridArea.removeChild(gridArea.firstChild);
-    }
+	while (gridArea.firstChild) {
+		gridArea.removeChild(gridArea.firstChild)
+	}
 }
 
 // Create the grid items with default number of squares
-drawGrid(squaresPerSide);
+drawGrid(squaresPerSide)
 
 // Button to change the number of squares
 
-const btn = document.createElement('button');
-btn.textContent = 'Squares per side?';
+const btn = document.createElement('button')
+btn.textContent = 'Clear grid'
 btn.addEventListener('click', () => {
-   squaresPerSide = prompt('How many squares per side?');
-   removeGrid();
-   drawGrid(squaresPerSide);
-   
+    while (true) {
+	    squaresPerSide = prompt('How many squares per side?')
+        if (squaresPerSide > 100) {
+            alert('Please enter a number between 1 and 100')
+        }
+        else {
+            break
+        }
+    }
+
+	removeGrid()
+	drawGrid(squaresPerSide)
 })
 
-const page = document.querySelector('body');
-page.insertBefore(btn, gridArea);
-
-
-
-
+const page = document.querySelector('body')
+page.insertBefore(btn, gridArea)
